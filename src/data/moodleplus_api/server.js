f112edc9f271592b838b14ed46aacdb18f211179
lloydilagan <c1-241-02669@uphsl.edu.ph>
2026-03-21 13:11:52 +0800

    Refactor server.js for improved readability

--- src/data/moodleplus_api/server.js
+++ src/data/moodleplus_api/server.js
@@ -34,5 +34,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
