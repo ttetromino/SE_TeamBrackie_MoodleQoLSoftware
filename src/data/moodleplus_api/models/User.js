b98820cf017c2cc5f53492699945cb4ee09e0a84
A_i_Delas_Alas <89058883+McLloydIlagan@users.noreply.github.com>
2026-03-27 21:27:37 +0800

    US-01-T-02: Biometrics Verification : User
    added biometrics fields to table

--- src/data/moodleplus_api/models/User.js
+++ src/data/moodleplus_api/models/User.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// US-01-T-03: Designing Database Schema 
-
 const userSchema = new mongoose.Schema({
   name: { 
     type: String, 
@@ -21,27 +19,58 @@ const userSchema = new mongoose.Schema({
     type: String, 
     required: true 
   },
-  lmsPassword: { type: String, required: true }
+  lmsPassword: { 
+    type: String, 
+    required: true 
+  },
+  // Session persistence fields
+  lmsCookies: {
+    type: [String],
+    default: []
+  },
+  lmsSessionExpiry: {
+    type: Date,
+    default: null
+  },
+  lmsLastLogin: {
+    type: Date,
+    default: null
+  },
+  // US-01-T-02: Biometrics Verification 
+  biometricEnabled: {
+    type: Boolean,
+    default: false
+  },
+  biometricToken: {
+    type: String,
+    default: null
+  },
+  biometricEnabledAt: {
+    type: Date,
+    default: null
+  }
 });
 
-
+// Hash passwords before saving
 userSchema.pre('save', async function() {
   console.log('Pre-save hook triggered');
   
-
   if (this.isModified('password')) {
     console.log('Hashing main password');
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
   }
   
-  
+  if (this.isModified('lmsPassword')) {
+    console.log('Hashing LMS password');
+    const salt = await bcrypt.genSalt(10);
+    this.lmsPassword = await bcrypt.hash(this.lmsPassword, salt);
+  }
   
   console.log('Pre-save hook completed');
-  
 });
 
-
+// Method to compare LMS password
 userSchema.methods.compareLMSPassword = async function(candidatePassword) {
   try {
     return await bcrypt.compare(candidatePassword, this.lmsPassword);
@@ -51,7 +80,7 @@ userSchema.methods.compareLMSPassword = async function(candidatePassword) {
   }
 };
 
-
+// Method to compare main password
 userSchema.methods.comparePassword = async function(candidatePassword) {
   try {
     return await bcrypt.compare(candidatePassword, this.password);
